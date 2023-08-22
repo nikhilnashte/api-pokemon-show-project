e7f501f32d290ece1c99bb7ade92fe1d225d8b32
nikhil nashte <nikhi.nashte@sciffer.com>
2023-08-22 12:44:35 +0530

    change

--- src/Routes/RouteFiles.js
+++ src/Routes/RouteFiles.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Router, Routes } from "react-router-dom"; 
 import Home from '../MiddleSection/Home'
 import SecondPage from '../MiddleSection/SecondPage';
